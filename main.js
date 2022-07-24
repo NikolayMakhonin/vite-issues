d5791eb7686908005ddfd786efb3d2927b57f238
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-07-24 09:33:38 +0400

    ssr import issue

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-import './style.css'
+import _asyncToGenerator from "@babel/runtime-corejs3/helpers/esm/asyncToGenerator";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+console.log("qwe".endsWith("e") + _asyncToGenerator);
