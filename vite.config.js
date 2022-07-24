674518b5246a37c0b7a3ddb7635176f0a96ca90f
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-07-24 08:17:37 +0400

    added babel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import babel from '@rollup/plugin-babel';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ command, mode }) => {
 	return {
@@ -8,7 +13,31 @@ export default defineConfig(({ command, mode }) => {
 				compilerOptions: {
 					hydratable: true /* required for clientside hydration */
 				}
-			})
+			}),
+			{
+				name: 'vite-plugin-babel',
+				config(config, config_env) {
+					return {
+						build: {
+							rollupOptions: {
+								plugins: [
+									babel.default({
+										configFile  : path.resolve(dirname, '.babelrc.cjs'), // enable babel for node_modules
+										extensions  : ['.ts', '.js', '.cjs', '.mjs', '.svelte', '.html'],
+										babelHelpers: 'runtime',
+										exclude     : [
+											// '**/node_modules/rollup*/**',
+											'**/node_modules/@babel/**',
+											'**/node_modules/core-js*/**',
+											'**/.svelte-kit/runtime/server/**'
+										],
+									}),
+								],
+							},
+						},
+					}
+				}
+			},
 		],
 		build: {
 			target: 'esnext',
